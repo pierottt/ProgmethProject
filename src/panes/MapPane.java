0b90bebf65c475477d8157ffa4fa1353d1fb9c4b
TaksaphornNow <taksaphorn.tpd@gmail.com>
2024-04-03 20:38:34 +0700

    Add Goto and MapPane

--- src/panes/MapPane.java
+++ src/panes/MapPane.java
@@ -4,7 +4,9 @@ import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
@@ -26,34 +28,52 @@ public class MapPane extends StackPane {
         Background background = new Background(backgroundImage);
         setBackground(background);
 
-        Text chicken = new Text("!");
-        Text dragon = new Text("!");
-        Text fox = new Text("!");
-        Text pikachu = new Text("!");
-        Text rat = new Text("!");
+        ImageView chickenView = createImageView("LocationGreen.png");
+        ImageView dragonView = createImageView("LocationGreen.png");
+        ImageView foxView = createImageView("LocationGreen.png");
+        ImageView pikachuView = createImageView("LocationGreen.png");
+        ImageView ratView = createImageView("LocationGreen.png");
+        ImageView homeView = createImageView("LocationGreen.png");
 
-        Font font = Font.font("Arial", FontWeight.BOLD, 14);
-        chicken.setFont(font);
-        dragon.setFont(font);
-        fox.setFont(font);
-        pikachu.setFont(font);
-        rat.setFont(font);
+        // Set image positions
+        StackPane.setAlignment(chickenView, Pos.TOP_CENTER);
+        StackPane.setMargin(chickenView, new Insets(190, 210, 0, 0));
+        StackPane.setAlignment(foxView, Pos.TOP_RIGHT);
+        StackPane.setMargin(foxView, new Insets(210, 300, 0, 0));
+        StackPane.setAlignment(dragonView, Pos.CENTER_LEFT);
+        StackPane.setMargin(dragonView, new Insets(0, 0, 100, 300));
+        StackPane.setAlignment(pikachuView, Pos.BOTTOM_LEFT);
+        StackPane.setMargin(pikachuView, new Insets(0, 0, 200, 350));
+        StackPane.setAlignment(ratView, Pos.BOTTOM_RIGHT);
+        StackPane.setMargin(ratView, new Insets(0, 250, 200, 0));
+        StackPane.setAlignment(homeView, Pos.CENTER);
+        StackPane.setMargin(homeView, new Insets(0, 60, 0, 0));
 
-        // Set text colors
-        chicken.setFill(Color.WHITE);
-        dragon.setFill(Color.WHITE);
-        fox.setFill(Color.WHITE);
-        pikachu.setFill(Color.WHITE);
-        rat.setFill(Color.WHITE);
+        getChildren().addAll(chickenView, dragonView, foxView, pikachuView, ratView, homeView);
+    }
 
-        // Set text positions
-        StackPane.setAlignment(chicken, Pos.TOP_LEFT);
-        StackPane.setAlignment(dragon, Pos.TOP_RIGHT);
-        StackPane.setAlignment(fox, Pos.BOTTOM_LEFT);
-        StackPane.setAlignment(pikachu, Pos.BOTTOM_RIGHT);
-        StackPane.setAlignment(rat, Pos.CENTER);
+    private ImageView createImageView(String imagePath) {
+        ImageView imageView = new ImageView(new Image(imagePath));
+        double imageSize = 60;
+        imageView.setFitWidth(imageSize);
+        imageView.setFitHeight(imageSize);
+        // Add drop shadow effect to images
+        DropShadow dropShadow = new DropShadow();
+        dropShadow.setColor(Color.rgb(0, 0, 0, 0.6)); // Shadow color with transparency
+        dropShadow.setRadius(10); // Blur radius
+        dropShadow.setSpread(0.5); // Shadow spread
+        dropShadow.setOffsetX(4); // Horizontal shadow offset
+        dropShadow.setOffsetY(4); // Vertical shadow offset
+        imageView.setEffect(dropShadow);
+        DropShadow dropShadowOn = new DropShadow();
+        dropShadowOn.setColor(Color.WHITE);
+        // Apply drop shadow effect on mouse hover
+        imageView.addEventHandler(MouseEvent.MOUSE_ENTERED, event -> imageView.setEffect(dropShadowOn));
 
-        getChildren().addAll(chicken, dragon, fox, pikachu, rat);
+        // Remove drop shadow effect on mouse exit
+        imageView.addEventHandler(MouseEvent.MOUSE_EXITED, event -> imageView.setEffect(dropShadow));
+
+        return imageView;
     }
 }
 
